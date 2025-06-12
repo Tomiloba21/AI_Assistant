8df4b3dc1451ea2f94d7ff315787eb95f624040a
Tomiloba <olowo15tomiloba@gmail.com>
2025-06-12 21:20:57 +0100

    another Update

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS dog;
-
-CREATE TABLE IF NOT EXISTS dog (
-                                   id          SERIAL PRIMARY KEY,
-                                   name        TEXT NOT NULL,
-                                   owner       TEXT NULL,
-                                   description TEXT NOT NULL
-);
--- src/test/java/dev/lobzter/assistant/AssistantApplicationTests.java
+++ src/test/java/dev/lobzter/assistant/AssistantApplicationTests.java
@@ -1,13 +1,29 @@
 package dev.lobzter.assistant;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @SpringBootTest
+
 class AssistantApplicationTests {
+
+
+	URI uri = UriComponentsBuilder.newInstance()
+			.path("inquire")
+			.buildAndExpand(1)
+			.toUri();
+
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
